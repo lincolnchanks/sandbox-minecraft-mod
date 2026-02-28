821b5477fea0a30ba8811e1a821a0ded4d3ab4a5
lincolnchanks <lchanks2024@gmail.com>
2026-02-28 15:05:00 -0700

    Initial commit

---
+++ src/main/java/com/erikkholdern/sandbox/SandboxClient.java
@@ -0,0 +1,10 @@
+package com.erikkholdern.sandbox;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SandboxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/erikkholdern/sandbox/SandboxDataGenerator.java
@@ -0,0 +1,11 @@
+package com.erikkholdern.sandbox;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SandboxDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
